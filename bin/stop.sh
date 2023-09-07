d02468ea580ad4f9a09174c48741783a20e65e36
Valery <nicemrlexx@live.ru>
2023-09-07 12:28:57 +0300

    Add secrets.env

--- bin/stop.sh
+++ bin/stop.sh
@@ -8,5 +8,5 @@ MAIN_PATH=`realpath $(dirname $SCRIPT)/..`
 becho "Stop docker"
 cd $MAIN_PATH/devilbox
 docker compose --env-file .env --env-file secrets.env stop
-docker compose rm -f &> /dev/null
+docker compose --env-file .env --env-file secrets.env rm -f &> /dev/null
 $MAIN_PATH/bin/config_remove_custom.sh
